fb92c98ec306c8456f87ebe60148c9f0ec61170a
Yasmin <yasmin@lit.ifce.edu.br>
2020-11-22 11:32:26 -0300

    unit test for get ranking

--- tests/Unit/MatchTeam/MatchTeamModelTest.php
+++ tests/Unit/MatchTeam/MatchTeamModelTest.php
@@ -4,6 +4,8 @@ namespace Tests\Unit\MatchTeam;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\Models\Team;
+use App\Models\MatchTeam;
 
 class MatchTeamModelTest extends TestCase
 {
@@ -17,7 +19,21 @@ class MatchTeamModelTest extends TestCase
 
     public function test_get_ranking_teams()
     {
-        
+        $team       = Team::first();
+        $matchTeams = MatchTeam::getRankingTeams($team->name);
+
+        $this->assertEquals(count($matchTeams),1);
+        $this->assertEquals($matchTeams->first()->name,$team->name);
+
+        $matchTeams = MatchTeam::getRankingTeams();
+        $this->assertEquals(count($matchTeams), count(
+                                                        MatchTeam::groupBy('team_id')
+                                                                  ->selectRaw('SUM(gols) as gols')
+                                                                  ->get()
+                                                     )
+                            );
+        $this->assertEquals($matchTeams->first()->ranking_position,1);
+
     }
   
 }
